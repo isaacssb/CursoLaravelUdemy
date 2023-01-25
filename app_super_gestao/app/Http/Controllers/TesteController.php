91e0b779a6869e5d3f0f848e26e8d4b32dfbd7a0
Isaac Barreto <isaacssb123@gmail.com>
2023-01-24 23:10:52 -0300

    test: router and controller

---
+++ app_super_gestao/app/Http/Controllers/TesteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    public function teste($p1, $p2)
+    {
+            return $p1 + $p2;
+    }
+}
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -19,7 +19,9 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
-Route::get('/login', function() {
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+Route::get('/login', function () {
     return 'login';
 })->name('site.login');
 
@@ -27,17 +29,16 @@ Route::get('/login', function() {
 //     echo 'parametro enviado por get ' . $id;
 // })->where('id', '[0-9]');
 
-Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function() {
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
         return 'clientes';
     })->name('app.clientes');
 
-    Route::get('/fornecedores', function() {
+    Route::get('/fornecedores', function () {
         return 'fornecedores';
     })->name('app.fornecedores');
 
-    Route::get('/produtos', function() {
+    Route::get('/produtos', function () {
         return 'produtos';
     })->name('app.produtos');
-
 });
