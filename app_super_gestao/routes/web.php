a714a5750e0842df22828da44469e92d67887524
Isaac Barreto <isaacssb123@gmail.com>
2023-01-31 22:55:17 -0300

    feat: form post page contato

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
-
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
